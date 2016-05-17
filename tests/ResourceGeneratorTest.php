cbfe1a43723f093f240c7f8829e219bb7b839177
Amo Chohan <amo.chohan@gmail.com>
2016-05-17 14:03:51 +0100

    Refactor tests

--- tests/ResourceGeneratorTest.php
+++ tests/ResourceGeneratorTest.php
@@ -1,23 +1,52 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\GenerateResource;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Tester\CommandTester;
 
 class ResourceGeneratorTest extends TestCase
 {
     /** @test */
     public function it_generates_a_new_model()
     {
-        // Given I am on the command line
-
         // When I run php artisan make:resource Animal
-        Artisan::call('make:resource', [
-            'name' => 'Animal'
-        ]);
+        $this->artisan('make:resource', ['name' => 'Animal']);
 
         // Then I see a new Model has been created in the App namespace
         // And it has the correct name
-        $this->assertTrue(file_exists(app_path('Animal.php')));
+        $this->seeFileWasCreated(app_path('/Animal.php'));
 
-        // And it has the correct fillable attributes
+        // Clear files created in the test
+        $this->removeCreatedFile(app_path('Animal.php'));
     }
+
+    /** @test */
+    public function it_does_not_overwrite_an_existing_model()
+    {
+        // Given there is an existing model called 'Animal'.
+        $this->runArtisanCommand(GenerateResource::class, ['name' => 'Animal']);
+
+        // And I try to create another model named 'Animal'.
+        $console = $this->runArtisanCommand(GenerateResource::class, ['name' => 'Animal']);
+
+        // Then I see an error
+        $this->seeInConsoleOutput('Model already exists!', $console);
+
+        // Clear up files created in the test
+        $this->removeCreatedFile(app_path('Animal.php'));
+    }
+
+    /** @test */
+    public function it_creates_a_model_with_the_correct_class_name()
+    {
+        // When I create a new model called 'Animal'.
+        $this->runArtisanCommand(GenerateResource::class, ['name' => 'Animal']);
+
+        // Then the model is created and the model has the correct class name
+        $this->seeInFile('class Animal', app_path('/Animal.php'));
+
+        // Clear up files created in the test
+        $this->removeCreatedFile(app_path('Animal.php'));
+    }
+
 }
